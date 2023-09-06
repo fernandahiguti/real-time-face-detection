c877631cf83155ae6b161b33b8ca917db7b383f0
Fernanda Higuti <fernandamh1996@gmail.com>
2023-09-06 18:24:41 -0300

    face detection

--- main.py
+++ main.py
@@ -44,3 +44,50 @@ def add_datetime_overlay(frame, margin=10):
     text_y = frame.shape[0] - margin
     cv2.putText(frame, datetime_text, (text_x, text_y), font, font_scale, font_color, thickness)
 
+def main():
+    face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    logo = load_logo('your_logo.png')  # Replace 'your_logo.png' with your logo's filename
+    margin = 10
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))  
+    output_folder = os.path.join(script_dir, 'face-detection-captures')  
+
+    os.makedirs(output_folder, exist_ok=True)
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+
+        logo_x = margin
+        logo_y = margin
+
+        alpha_channel = None
+        if logo.shape[2] == 4:
+            alpha_channel = logo[:, :, 3]
+
+        for c in range(0, 3):
+            frame[logo_y:logo_y + logo.shape[0], logo_x:logo_x + logo.shape[1], c] = \
+                frame[logo_y:logo_y + logo.shape[0], logo_x:logo_x + logo.shape[1], c] * \
+                (1 - alpha_channel / 255.0) + logo[:, :, c] * (alpha_channel / 255.0) if alpha_channel is not None else logo[:, :, c]
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 0)
+
+        add_datetime_overlay(frame, margin)
+
+        cv2.imshow('Video Face Detection', frame)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+        elif key == ord('c'):
+            capture_and_save(frame, output_folder)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
