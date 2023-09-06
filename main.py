5e0d5ca9c3ab50bc33bfdb1240d477e1cda4cce4
Fernanda Higuti <fernandamh1996@gmail.com>
2023-09-06 18:18:09 -0300

    load logo function

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ from datetime import datetime
 import pytz
 import tzlocal
 
+def load_logo(logo_path):
+    if os.path.exists(logo_path):
+        logo = cv2.imread(logo_path, cv2.IMREAD_UNCHANGED)
+        logo = cv2.resize(logo, (36, 36))
+        return logo
+    else:
+        return create_placeholder_logo()
+
