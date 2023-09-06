9cb7b3cfbbf7289d9b12751f505040648233b465
Fernanda Higuti <fernandamh1996@gmail.com>
2023-09-06 18:23:50 -0300

    add datetime overlay

--- main.py
+++ main.py
@@ -29,3 +29,18 @@ def capture_and_save(frame, output_folder):
     cv2.imwrite(file_name, frame)
     print(f'Image saved as {file_name}')
 
+def add_datetime_overlay(frame, margin=10):
+    local_timezone = tzlocal.get_localzone()
+    current_time = datetime.now(local_timezone).strftime('%Y-%m-%d %H:%M:%S')
+
+    datetime_text = f'{current_time} {local_timezone}'
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.5
+    font_color = (255, 255, 255) 
+    thickness = 1
+    text_size = cv2.getTextSize(datetime_text, font, font_scale, thickness)[0]
+    text_x = frame.shape[1] - text_size[0] - margin
+    text_y = frame.shape[0] - margin
+    cv2.putText(frame, datetime_text, (text_x, text_y), font, font_scale, font_color, thickness)
+
