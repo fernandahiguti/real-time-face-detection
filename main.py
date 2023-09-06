6640f3035b0e03f6bb07de69fbc816d3000a17dd
Fernanda Higuti <fernandamh1996@gmail.com>
2023-09-06 18:22:17 -0300

    add margin to logo

--- main.py
+++ main.py
@@ -17,3 +17,9 @@ def create_placeholder_logo():
     logo = np.ones((36, 36, 4), dtype=np.uint8) * 192 
     return logo
 
+def add_margin(image, margin):
+    if image.shape[2] == 4:
+        return np.zeros((image.shape[0] + 2 * margin, image.shape[1] + 2 * margin, 4), dtype=np.uint8)
+    else:
+        return np.zeros((image.shape[0] + 2 * margin, image.shape[1] + 2 * margin, 3), dtype=np.uint8)
+
